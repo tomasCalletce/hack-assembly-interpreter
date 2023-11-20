7bfc1873307d52d5e74afb57967a1de70ebe995b
Jacoroch <110855576+Jacoroch@users.noreply.github.com>
2023-11-20 17:08:35 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -17,13 +17,17 @@ from gates.AluGate import AluGate
 from gates.BitGate import BitGate
 from gates.RegisterGate import RegisterGate
 from gates.PcGate import PcGate
+from gates.Mux4Way16Gate import Mux4Way16Gate
+from gates.Mux8Way16Gate import Mux8Way16Gate
+from gates.DMuxGate import DMuxGate
+from gates.DMux4WayGate import DMux4WayGate
+from gates.DMux8WayGate import DMux8WayGate
 
+#nand = NandGate()
+#print(nand(True, True))
 
-# nand = NandGate()
-# print(nand(True, True))
-
-# not_gate = NotGate()
-# print(not_gate(True))
+#not_gate = NotGate()
+#print(not_gate(True))
 
 # and_gate = AndGate()
 # print(and_gate(True, True))
@@ -34,13 +38,67 @@ from gates.PcGate import PcGate
 # xor_gate = XorGate()
 # print(xor_gate(True, True))
 
-# mux_gate = MuxGate()
-# print(mux_gate(True, False, False))
+#mux_gate = MuxGate()
+#print(mux_gate(True, False, False))
 
-# mux16_gate = Mux16Gate()
-# input_a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-# input_b = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-# print (mux16_gate(input_a, input_b, True))
+#mux16_gate = Mux16Gate()
+input_a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+input_b = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+input_c = [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+input_d = [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]
+input_e = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1] 
+input_f = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]  
+input_g = [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1] 
+input_h = [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0]
+#print (mux16_gate(input_a, input_b, False))
+
+#mux4way16_gate = Mux4Way16Gate()
+#print(mux4way16_gate(input_a,  input_b, input_c, input_d, [0,1]))
+
+#Mux8way16
+'''
+mux8way16_gate = Mux8Way16Gate()
+print(mux8way16_gate(input_a, input_b, input_c, input_d, input_e, input_f, input_g, input_h, [False, False, False]))  # Debería seleccionar 'a'
+print(mux8way16_gate(input_a, input_b, input_c, input_d, input_e, input_f, input_g, input_h, [False, False, True]))   # Debería seleccionar 'b'
+print(mux8way16_gate(input_a, input_b, input_c, input_d, input_e, input_f, input_g, input_h, [False, True, False]))   # Debería seleccionar 'c'
+print(mux8way16_gate(input_a, input_b, input_c, input_d, input_e, input_f, input_g, input_h, [False, True, True]))    # Debería seleccionar 'd'
+print(mux8way16_gate(input_a, input_b, input_c, input_d, input_e, input_f, input_g, input_h, [True, False, False]))   # Debería seleccionar 'e'
+print(mux8way16_gate(input_a, input_b, input_c, input_d, input_e, input_f, input_g, input_h, [True, False, True]))    # Debería seleccionar 'f'
+print(mux8way16_gate(input_a, input_b, input_c, input_d, input_e, input_f, input_g, input_h, [True, True, False]))    # Debería seleccionar 'g'
+print(mux8way16_gate(input_a, input_b, input_c, input_d, input_e, input_f, input_g, input_h, [True, True, True]))     # Debería seleccionar 'h'
+'''
+#dmux_gate= DMuxGate()
+#print(dmux_gate(1, 1))
+
+#dmux4way
+'''
+def test_dmux4way_gate():
+    dmux4way_gate = DMux4WayGate()
+    
+    test_cases = [(0, (0, 0)), (0, (0, 1)), (0, (1, 0)), (0, (1, 1)),
+                  (1, (0, 0)), (1, (0, 1)), (1, (1, 0)), (1, (1, 1))]
+
+    for input, select in test_cases:
+        a, b, c, d = dmux4way_gate(input, select)
+        print(f"input: {input}, select: {select} -> a: {a}, b: {b}, c: {c}, d: {d}")
+test_dmux4way_gate()
+'''
+#DMux8Way
+'''
+def test_dmux8way_gate():
+    dmux8way_gate = DMux8WayGate()
+
+    test_cases = [(input, (sel0, sel1, sel2))
+                  for input in [0, 1]
+                  for sel0 in [False, True]
+                  for sel1 in [False, True]
+                  for sel2 in [False, True]]
+
+    for input, select in test_cases:
+        outputs = dmux8way_gate(input, select)
+        print(f"input: {input}, select: {select} -> outputs: {outputs}")
+test_dmux8way_gate()
+'''
 
 # half_adder_gate = HalfAdderGate()
 # sum_result, carry_out = half_adder_gate(1, 1)
@@ -68,8 +126,8 @@ from gates.PcGate import PcGate
 # res = or8way_gate(input_one)
 # print(res)
 
-input_one = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
-input_two = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1]
+#input_one = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+#input_two = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1]
 
 # alu_out, zr_out, ng_out = AluGate()(input_one, input_two, 0, 1, 0, 0, 1, 1)
 
