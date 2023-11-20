dda655401cebeb9f7e48f2d6e60a0a8fa9f0414c
Jacoroch <110855576+Jacoroch@users.noreply.github.com>
2023-11-20 17:04:47 -0500

    Create DMuxGate.py

---
+++ src/gates/DMuxGate.py
@@ -0,0 +1,14 @@
+from .AndGate import AndGate
+from .NotGate import NotGate
+
+class DMuxGate:
+    def __init__(self):
+        self.and_gate = AndGate()
+        self.not_gate = NotGate()
+
+    def __call__(self, input, select):
+        # if select is 0 return a otherwise return b
+        not_select = self.not_gate(select)
+        out_a = self.and_gate(input, not_select)
+        out_b = self.and_gate(select, input)
+        return out_a, out_b
